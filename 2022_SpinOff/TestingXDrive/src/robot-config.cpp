9a01312c13dbfb5a7768a12d1046753d5b9f858e
nchithan <chithannaveen@gmail.com>
2022-05-11 19:30:30 -0400

    Testing XDrive

---
+++ 2022_SpinOff/TestingXDrive/src/robot-config.cpp
@@ -0,0 +1,38 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor driveFrontLeft = motor(PORT16, ratio18_1, true);
+motor driveMiddleLeft = motor(PORT18, ratio18_1, true);
+motor driveBackLeft = motor(PORT19, ratio18_1, true);
+motor driveFrontRight = motor(PORT2, ratio18_1, false);
+motor driveMiddleRight = motor(PORT10, ratio18_1, false);
+motor driveBackRight = motor(PORT5, ratio18_1, false);
+inertial Gyro = inertial(PORT9);
+motor backClawLeft = motor(PORT17, ratio18_1, true);
+motor backClawRight = motor(PORT8, ratio18_1, false);
+motor frontLiftRight = motor(PORT4, ratio18_1, false);
+motor frontLiftLeft = motor(PORT15, ratio18_1, true);
+motor Intake = motor(PORT1, ratio18_1, false);
+digital_out claw = digital_out(Brain.ThreeWirePort.H);
+digital_out rightPiston = digital_out(Brain.ThreeWirePort.B);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
